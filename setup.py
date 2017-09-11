20ee96b21716c64d06fd0b439a8249d61b06635b
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2017-09-11 07:28:59 +0200

    Add scripts folder in setuptools

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@ setup(
     name='abrox',
     packages=find_packages(),
     package_data={'abrox': ['gui/icons/*']},
-    version='0.0.8',
+    version='0.1.0',
     license='MIT',
     description='A GUI for Approximate Bayesian Computation',
     long_description=open('README.md').read(),
     author='Ulf Mertens',
     author_email='mertens.ulf@gmail.com',
-    entry_points={
-        'gui_scripts': [
-            'abrox-gui = abrox.gui.__main__:main'
-        ]
-    },
+    scripts=['scripts/abrox-gui'],
+    # entry_points={
+    #       'gui_scripts': [
+    #           'abrox-gui = abrox.gui.__main__:main'
+    #       ]
+    #   },
     url='https://github.com/mertensu/ABrox',  # use the URL to the github repo
     download_url='https://github.com/mertensu/ABrox/archive/0.1.tar.gz',
     setup_requires=['numpy'],
