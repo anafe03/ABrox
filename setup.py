94197830468902b8aa80667be7a9698299a43dc0
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2017-09-26 11:03:26 +0200

    Add template for g and k distribution

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='abrox',
     packages=find_packages(),
     package_data={'abrox': ['gui/icons/*']},
-    version='0.1.4',
+    version='0.2.0',
     license='MIT',
     description='A tool for Approximate Bayesian Computation',
     long_description=open('README.md').read(),
