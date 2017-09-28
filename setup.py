8a1575e91fb47046a7f2df8722ec4cc4ea85cfed
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2017-09-28 10:51:08 +0200

    Change version for PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='abrox',
     packages=find_packages(),
     package_data={'abrox': ['gui/icons/*']},
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description='A tool for Approximate Bayesian Computation',
     long_description=open('README.md').read(),
