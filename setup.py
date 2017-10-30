f2330d6ffb12a849c8cea87f884f92aa21a97003
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2017-10-30 10:45:11 +0100

    Add init to icons folder

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='abrox',
     packages=find_packages(),
     package_data={'abrox': ['gui/icons/*']},
-    version='0.9.7',
+    version='0.9.9',
     license='MIT',
     description='A tool for Approximate Bayesian Computation',
     long_description=open('README.md').read(),
