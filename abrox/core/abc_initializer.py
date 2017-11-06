46fc898a63e945f2d512dc3cbd15cf289ea6d1e0
Stefan Radev <stefan.radev93@gmail.com>
2017-11-06 14:38:55 +0100

    Cleaned initializer

--- abrox/core/abc_initializer.py
+++ abrox/core/abc_initializer.py
@@ -17,24 +17,12 @@ class ABCInitializer:
         """
 
         if self.config['data']['datafile'] is not None:
-            return self.loadExternalData()
+            return self._loadExternalData()
         else:
             for i, model in enumerate(model):
                 if i == self.config['settings']['test']['model']:
                     params = self.config['settings']['test']['parameter']
                     return model.simulate(params)
-
-    def loadExternalData(self):
-        """
-        Import external dataset.
-        :return: the dataset
-        """
-        try:
-            return pd.read_csv(self.config['data']['datafile'],
-                               delimiter=self.config['data']['delimiter']).as_matrix()
-
-        except ImportError('Imported data could not be imported'):
-            return None
 
     def buildAndGetModels(self):
         """
@@ -48,55 +36,12 @@ class ABCInitializer:
 
         return self.model, modelNames
 
-    def getSummaryFunc(self):
-        """Returns an instance of summary class."""
-        return self.config['summary']
-
-    def _flattenList(self, alist):
-        return list(chain.from_iterable(alist))
-
-    def getSimSettings(self):
-        """ Get number of simulations and how many should be accepted. """
-        simulations = self.config['settings']['preprocess']['simulations']
-        keep = self.config['settings']['preprocess']['keep']
-        threshold = self.config['settings']['preprocess']['threshold']
-
-        return simulations, keep, threshold
-
-    def getObjective(self):
-        """
-        Returns the objective. Either comparison or inference.
-        """
-        return self.config['settings']['objective']
-
-    def getModelNumber(self):
-        """
-        Returns the number of models specified.
-        """
-        return len(self.config['models'])
-
-    def getParameterNames(self):
-        """
-        Returns parameter names of first model only!!
-        """
-        return self._flattenList([list(d.keys()) for d in self.config['models'][0]['prior']])
-
-    def getAlgorithmInfo(self):
-        """
-        Get algorithm and additional hyperparameters.
-        """
-        info = self.config['settings']['method']
-        algo = info['algorithm']
-        algoParams = info['specs']
-
-        return algo, algoParams
-
     def extractAndGetSettings(self):
         """Extracts the relevant preporcessing and algorithm settings."""
 
         algo = self.config['settings']['method']['algorithm']
         specs = self.config['settings']['method']['specs']
-        paramNames = self.getParameterNames()
+        paramNames = self._getParameterNames()
         objective = self.config['settings']['objective']
         nModels = len(self.config['models'])
         simulations = self.config['settings']['preprocess']['simulations']
@@ -113,6 +58,34 @@ class ABCInitializer:
                     'tr': threshold}
 
         return settings
+
+    def getSummaryFunc(self):
+        """Returns an instance of summary class."""
+        return self.config['summary']
+
+    def _flattenList(self, alist):
+        return list(chain.from_iterable(alist))
+
+    def _getParameterNames(self):
+        """
+        Returns parameter names of first model only!!
+        """
+        return self._flattenList([list(d.keys()) for d in self.config['models'][0]['prior']])
+
+    def _loadExternalData(self):
+        """
+        Import external dataset.
+        :return: the dataset
+        """
+        try:
+            return pd.read_csv(self.config['data']['datafile'],
+                               delimiter=self.config['data']['delimiter']).as_matrix()
+
+        except ImportError('Imported data could not be imported'):
+            return None
+
+
+
 
 
 
