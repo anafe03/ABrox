33a6684d815da2106e10af39c73d56d8a3af0f42
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2018-02-07 11:52:33 +0100

    Add settings to NN

--- tests/test_hierarchical_normal.py
+++ tests/test_hierarchical_normal.py
@@ -9,17 +9,17 @@ from abrox.core.abc_utils import plotLosses
 
 
 def summary(data):
-    # mean = np.mean(data)
-    # variance = np.var(data)
-    # mad = np.median(np.absolute(data - np.median(data)))
-    # raw = np.array([mean,variance,mad])
-    # all2sum = np.array([np.sum(tup) for tup in combinations(raw, 2)])
-    # all2prod = np.array([np.prod(tup) for tup in combinations(raw, 2)])
-    # all3sum = np.reshape(np.sum(raw),1)
-    # all3prod = np.reshape(np.prod(raw),1)
-    # noise = np.random.uniform(size=50)
-    # return np.concatenate((raw,all2sum,all2prod,all3sum,all3prod,noise))
-    return np.sort(data)
+    mean = np.mean(data)
+    variance = np.var(data)
+    mad = np.median(np.absolute(data - np.median(data)))
+    raw = np.array([mean,variance,mad])
+    all2sum = np.array([np.sum(tup) for tup in combinations(raw, 2)])
+    all2prod = np.array([np.prod(tup) for tup in combinations(raw, 2)])
+    all3sum = np.reshape(np.sum(raw),1)
+    all3prod = np.reshape(np.prod(raw),1)
+    noise = np.random.uniform(size=50)
+    return np.concatenate((raw,all2sum,all2prod,all3sum,all3prod,noise))
+    # return np.sort(data)
 
 
 def simulate_Model1(params):
@@ -48,9 +48,17 @@ CONFIG = {
     "settings": {
         'distance_metric': 'default',
         'method': {'algorithm': 'nn',
-                   'specs': {}},
+                   'specs': {'hidden_layers': 3,
+                             'hidden_layer_sizes': [0.8,0.6,0.4],
+                             'activation': 'relu',
+                             'bn': False,
+                             'loss': 'mean_squared_error',
+                             'optimizer': 'adam',
+                             'epochs': 10,
+                             'val_size': 0.05,
+                             'load_model': None}},
         'objective': 'inference',
-        'outputdir': '.',
+        'outputdir': '/Users/ulf.mertens/Desktop/nndl/',
         'reftable': {'extref': None, 'simulations': int(10e4)},
         'test': {'fixed': {'mu': 0, 'sigma': 1}, 'model': 0}
     }
@@ -62,6 +70,7 @@ def samplesFromTruePosterior(data):
     rawMean = data.mean()
     s2 = np.sum((data - rawMean) ** 2)
     N = len(data)
+    print("N: ", N)
     truePosteriorMean = stats.t(df=N + 8, loc=(N * rawMean) / (N + 1), scale=(s2 + 6) / ((N + 1) * (N + 8)))
     truePosteriorVariance = stats.invgamma(a=54, scale=(N / 2) * (s2 / N) + 3)
 
@@ -75,55 +84,61 @@ def samplesFromTruePosterior(data):
 
 if __name__ == "__main__":
 
-    keras_model = load_model('/Users/ulf.mertens/Desktop/nndl/my_model.h5')
+    # keras_model = load_model('/Users/ulf.mertens/Desktop/nndl/my_model.h5')
 
     abc = Abc(CONFIG)
-    raw, loss, val_loss, ps = abc.run(keras_model)
-
-
+    raw, loss, val_loss, ps = abc.run()
 
     PATH = '/Users/ulf.mertens/Desktop/nndl/'
 
-    sim = 100
+    # sim = 100
+    #
+    # final = np.empty(shape=(sim,8))
+    #
+    # keras_model = load_model('/Users/ulf.mertens/Desktop/nndl/my_model.h5')
+    #
+    # for i in range(sim):
+    #     CONFIG['settings']['test']['fixed']['sigma'] = stats.invgamma(a=4,scale=3).rvs()
+    #     CONFIG['settings']['test']['fixed']['mu'] = stats.norm(loc=0,scale=CONFIG['settings']['test']['fixed']['sigma']).rvs()
+    #
+    #     abc = Abc(CONFIG)
+    #     raw, loss,val_loss, ps = abc.run(keras_model)
+    #
+    #     #plotLosses(loss,val_loss,PATH)
+    #
+    #     expMean, expMeanVar, expVar, expVarVar = samplesFromTruePosterior(raw)
+    #
+    #     print("Running simulation {}/{}".format(i+1,sim))
+    #
+    #     final[i,0] = expMean #raw.mean()
+    #     final[i,1] = ps[:,0].mean()
+    #     final[i,2] = expVar #raw.var()
+    #     final[i,3] = ps[:,1].mean()
+    #     final[i,4] = expMeanVar
+    #     final[i,5] = ps[:,0].var()
+    #     final[i,6] = expVarVar
+    #     final[i,7] = ps[:, 1].var()
 
-    final = np.empty(shape=(sim,8))
+    plotLosses(loss, val_loss, PATH)
 
-    keras_model = load_model('/Users/ulf.mertens/Desktop/nndl/my_model.h5')
+    expMean, expMeanVar, expVar, expVarVar = samplesFromTruePosterior(raw)
 
-    for i in range(sim):
-        CONFIG['settings']['test']['fixed']['sigma'] = stats.invgamma(a=4,scale=3).rvs()
-        CONFIG['settings']['test']['fixed']['mu'] = stats.norm(loc=0,scale=CONFIG['settings']['test']['fixed']['sigma']).rvs()
+    print("actual mean: ", raw.mean())
+    print("actual var: ", raw.var())
 
-        abc = Abc(CONFIG)
-        raw, loss,val_loss, ps = abc.run(keras_model)
+    print("True var mean: ", np.array(expMeanVar))
+    print("Est. var mean: ", ps[:,0].var)
 
-        #plotLosses(loss,val_loss,PATH)
+    plt.hist(ps[:,0])
+    plt.axvline(expMean, color='b', linestyle='dashed', linewidth=2)
+    plt.savefig(PATH + 'mean.png', bbox_inches='tight')
+    plt.clf()
 
-        expMean, expMeanVar, expVar, expVarVar = samplesFromTruePosterior(raw)
+    # VAR hist
 
-        print("Running simulation {}/{}".format(i+1,sim))
+    plt.hist(ps[:, 1])
+    plt.axvline(expVar, color='b', linestyle='dashed', linewidth=2)
+    plt.savefig(PATH + 'sigma.png', bbox_inches='tight')
+    plt.clf()
 
-        final[i,0] = expMean #raw.mean()
-        final[i,1] = ps[:,0].mean()
-        final[i,2] = expVar #raw.var()
-        final[i,3] = ps[:,1].mean()
-        final[i,4] = expMeanVar
-        final[i,5] = ps[:,0].var()
-        final[i,6] = expVarVar
-        final[i,7] = ps[:, 1].var()
-
-        ## Mean hist
-
-        # plt.hist(samplesMean, normed=True)
-        # plt.hist(ps[:,0],normed=True)
-        # plt.savefig('/Users/ulf.mertens/Desktop/nndl/mean.png', bbox_inches='tight')
-        # plt.clf()
-
-        ## VAR hist
-
-        # plt.hist(samplesVar,normed=True)
-        # plt.hist(ps[:, 1], normed=True)
-        # plt.savefig('/Users/ulf.mertens/Desktop/nndl/sigma.png', bbox_inches='tight')
-        # plt.clf()
-
-    np.savetxt(PATH + 'results.csv',final)
+    #np.savetxt(PATH + 'results.csv',final)
