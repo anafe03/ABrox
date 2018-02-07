5f5a5177708add6de2752afda5326c29829621d6
Ulf Mertens <ulf.mertens@psychologie.uni-heidelberg.de>
2018-02-07 11:52:13 +0100

    Use Keras functional API

--- abrox/core/abc_neural_net.py
+++ abrox/core/abc_neural_net.py
@@ -1,140 +1,172 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential, Model
+from keras import layers
+from keras import Input
 from abrox.core.abc_utils import toArray, cross_val
-from keras.layers.core import Lambda
 from keras import backend as K
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+from keras.utils import plot_model
+
+
+from abrox.core.abc_concrete_dropout import ConcreteDropout
 
 
 class ABCNeuralNet:
     """Implements a random forest for ABC model selection."""
 
-    def __init__(self, refTable, preprocessor, objective):
+    def __init__(self, refTable, preprocessor, settings):
 
         self._refTable = refTable
         self._pp = preprocessor
-        self.objective = objective
+        self._nnSettings = settings['specs']
+        self.outputdir = settings['outputdir']
+        self.X_train = None
+        self.y_train = None
+        self.X_val = None
+        self.y_val = None
+        self.dropout = None
+        self.hyperParams = None
 
-    def run(self,rawData,model):
-        """Runs according to settings (these must be specified by user.)"""
+    def constructTrainAndValSet(self):
+        """
+        Construct training and validation set.
+        :return: None
+        """
+        X = toArray(self._refTable, 'sumstat')
+        y = toArray(self._refTable, 'param')
+        self.X_train, self.X_val, self.y_train, self.y_val = \
+            train_test_split(X, y, test_size=self._nnSettings['val_size'])
 
-        if self.objective == 'comparison':
+    def normalize(self):
+        """
+        Normalize features.
+        :return: fitted scaler
+        """
+        scaler = preprocessing.StandardScaler().fit(self.X_train)
+        scaler.transform(self.X_train,copy=False)
+        scaler.transform(self.X_val,copy=False)
+        return scaler
 
-            # Extract sum stats and model indices from ref table
-            indices = toArray(self._refTable, 'idx').flatten()
-            sumStat = toArray(self._refTable, 'sumstat')
+    def setConcreteDropout(self):
+        """
+        Choose hyperparameters for Concrete Dropout.
+        :return:
+        """
+        l = 1e-4
+        wd = l ** 2. / (self.X_train.shape[0])
+        dd = 2. / (self.X_train.shape[0])
+        self.hyperParams = wd, dd
 
-            print(sumStat.shape)
+    def add_layers(self, size, prev_tensor):
+        """
+        Add dense layer with concrete dropout.
+        :param size: number of nodes in hidden layer
+        :param prev_tensor: the previous tensor
+        :return: the final layer
+        """
+        wd, dd = self.hyperParams
+        return ConcreteDropout(
+            layers.Dense(size, activation=self._nnSettings['activation']),
+            weight_regularizer=wd, dropout_regularizer=dd)(prev_tensor)
 
-            # Create a classifier
-            # TODO according to user-specified settings
-            # TODO 2: Implement random forest without sklearn dependency
+    def build_keras_model(self):
+        """
+        Build a simple Keras model.
+        :return: the model
+        """
+        features = self.X_train.shape[1]
+        outputs = self.y_train.shape[1]
+        input_tensor = Input(shape=(features, ))
+        x = input_tensor
 
-            model = Sequential()
-            model.add(Dense(10, input_dim=sumStat.shape[1], kernel_initializer='glorot_uniform', activation='relu'))
-            model.add(Dense(10, kernel_initializer='glorot_uniform', activation='relu'))
-            model.add(Dense(1, kernel_initializer='glorot_uniform', activation='sigmoid'))
-            # Compile model
-            model.compile(loss='binary_crossentropy', optimizer='adam')
+        for i in range(self._nnSettings['hidden_layers']):
+            units = int(self._nnSettings['hidden_layer_sizes'][i] * features)
+            x = self.add_layers(units, x)
 
-            # Do a 5-fold cross-validation
-            # accuracies = cross_val(sumStat, indices, model, 5)
-            # print("Neural net cross-val accuracies: ")
-            # print(accuracies)
+        output_tensor = layers.Dense(outputs, activation='linear')(x)
 
-            # Fit on summary statistics (the more the better)
-            model.fit(sumStat, indices, batch_size=64, epochs=2, shuffle=True, validation_split=0.2)
+        return Model(input_tensor, output_tensor)
 
-            # Predict probabilities of models on summary obs
-            sumStatTest = np.array(self._pp.scaledSumStatObsData).reshape(1, -1)
-            print("Probability of model 1 is: \n")
-            pred = model.predict_proba(sumStatTest)
+    def storeDropoutRates(self, model):
+        """
+        Return the optimized dropout probabilities.
+        :param model: the keras model
+        :return: the probabilities
+        """
+        dropoutProbs = []
+        for i in range(self._nnSettings['hidden_layers'] -1):
+            dropoutProbs.append(K.eval(K.exp(model.layers[i].weights[2])))
+        self.dropout = dropoutProbs
 
-            return pred
+    def makePredictions(self, model, scaler, outputs, N=500):
+        """
+        Predict at observed data using Dropout at test-time.
+        :param model: keras model
+        :param scaler: the scaler used to scale training set
+        :param outputs: number of output units
+        :param N: predictions
+        :return: the samples from the posterior predictive.
+        """
 
-        else:
+        sumStatTest = np.array(self._pp.scaledSumStatObsData).reshape(1, -1)
+        sumStatTest = scaler.transform(sumStatTest)
 
-            X = toArray(self._refTable, 'sumstat')
-            y = toArray(self._refTable, 'param')
+        posteriorSamples = np.empty(shape=(N, outputs))
+        for i in range(N):
+            posteriorSamples[i,] = model.predict(sumStatTest)
 
-            print(X.shape)
-            print(y.shape)
+        return posteriorSamples
 
-            X_val = X[:5000]
-            X_train = X[5000:]
+    def run(self,rawData):
+        """
+        Run the whole procedure and return the predictions with uncertainty.
+        :param rawData:
+        :return:
+        """
 
-            y_val = y[:5000]
-            y_train = y[5000:]
+        self.constructTrainAndValSet()
 
-            print('Normalizing...')
-            scaler = preprocessing.StandardScaler().fit(X_train)
+        features = self.X_train.shape[1]
+        outputs = self.y_train.shape[1]
 
-            X_train = scaler.transform(X_train)
-            X_val = scaler.transform(X_val)
+        print("Training size: ", self.X_train.shape[0])
+        print("Validation size: ", self.X_val.shape[0])
+        print("Running model on {} features".format(features))
+        print("Predicting {} parameters".format(outputs))
 
-            features = X.shape[1]
-            outputs = y.shape[1]
-            print("Running model on {} features".format(features))
-            print("Prediction {} parameters".format(outputs))
+        self.setConcreteDropout()
 
-            load = True
-            if load:
-                wkrt = 1 # nonesense
+        scaler = self.normalize()
 
-            else:
+        if self._nnSettings['load_model'] is not None:
+            model = load_model(self.outputdir + self._nnSettings['load_model'])
 
-                model = Sequential()
-                model.add(Dense(features, input_shape=(features,), activation='relu'))
-                model.add(Lambda(lambda x: K.dropout(x, level=0.1)))
-                model.add(Dense(features, activation='relu'))
-                model.add(Lambda(lambda x: K.dropout(x, level=0.1)))
-                model.add(Dense(features, activation='relu'))
-                model.add(Lambda(lambda x: K.dropout(x, level=0.1)))
-                model.add(Dense(y.shape[1], activation='linear'))
-
-                # Compile model
-                model.compile(loss='mean_squared_error', optimizer='adam')
-
-                # Fit model
-                history = model.fit(x=X_train, y=y_train,
-                                    batch_size=32,
-                                    epochs=20,
-                                    shuffle=True,
-                                    validation_data=(X_val,y_val),
-                                    verbose=True)
-
-                # Save model
-                model.save('/Users/ulf.mertens/Desktop/nndl/my_model.h5')
-
-                print("Finished fitting")
-
-            sumStatTest = np.array(self._pp.scaledSumStatObsData).reshape(1, -1)
-            sumStatTest = scaler.transform(sumStatTest)
-
-            print('Prediction with dropout...')
-
-            N = 100
-            posteriorSamples = np.empty(shape=(N,outputs))
-            for i in range(N):
-                posteriorSamples[i,] = model.predict(sumStatTest)
-
-            print('Finished predicting...')
-
-            loss = None #history.history['loss']
-            val_loss = None  #history.history['val_loss']
-
-            return rawData, loss, val_loss, posteriorSamples
-
+        model = self.build_keras_model()
 
 
-# convModel = models.Sequential()
-# convModel.add(layers.Conv1D(32,160,strides=12,activation='relu',input_shape=(48,1)))
-# convModel.add(layers.MaxPooling1D(pool_size=2))
-# convModel.add(layers.Conv1D(32,40,strides=8,activation='relu'))
-# convModel.add(layers.MaxPooling1D(pool_size=2))
-# convModel.add(layers.Conv1D(64,80,strides=2,activation='relu'))
-# convModel.add(layers.MaxPooling1D(pool_size=2))
-# convModel.add(layers.Flatten())
-# convModel.add(layers.Dense(64,activation='relu'))
-# convModel.add(layers.Dense(2,activation='linear'))
+        #plot_model(model, to_file=self.outputdir + 'model.png')
+        model.summary()
+
+        model.compile(loss=self._nnSettings['loss'], optimizer=self._nnSettings['optimizer'])
+
+        # Fit model
+        history = model.fit(x=self.X_train,
+                            y=self.y_train,
+                            batch_size=32,
+                            epochs=self._nnSettings['epochs'],
+                            validation_data=(self.X_val,self.y_val),
+                            verbose=False)
+
+        # Save model
+        model.save(self.outputdir + 'keras_model.h5')
+
+        # self.storeDropoutRates(model)
+
+        posteriorSamples = self.makePredictions(model,scaler,outputs)
+
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+        return rawData, loss, val_loss, posteriorSamples
